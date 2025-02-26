fef94ceed703223a012b8ac9638ba6f6c92f0318
chaianuntotko <168754350+chaianuntotko@users.noreply.github.com>
2025-02-26 19:56:34 +0700

    Add files via upload

--- client/src/pages/admin/AdminHomePage.jsx
+++ client/src/pages/admin/AdminHomePage.jsx
@@ -3,13 +3,15 @@ import { useEffect, useState } from 'react'
 import { Link, Outlet, useNavigate } from 'react-router-dom'
 import UTCC from '../../assets/UTCC.png'
 
+import HeadAdmin from './components/HeadAdmin.jsx'
+
 
 const AdminHomePage = () => {
   
   const navigate = useNavigate();
   const [userName, setUserName] = useState("")
 
-  useEffect(() => {
+  /*useEffect(() => {
     const checkAuth = async () => {
       try {
         const res = await axios.get('/auth/protected', {withCredentials: true})
@@ -21,7 +23,7 @@ const AdminHomePage = () => {
     }
 
     checkAuth()
-  }, [navigate])
+  }, [navigate])*/
 
   const handleLogout = async () => {
     try {
@@ -37,36 +39,40 @@ const AdminHomePage = () => {
 
       {/* Sidebar */}
 
-      <div className='w-[15%] bg-[#D9D143] p-5 h-screen flex justify-evenly text-center flex-col'>
-        <ul>
-
-          <li className='border-b-4 border-white py-5 px-5'>
-            <Link to="register" className='font-semibold hover:text-pink-600'>ลงทะเบียนนักศึกษา</Link>
+      <div className='w-[15%] bg-[#163D76] p-5 h-screen flex justify-evenly text-left flex-col'>
+        <ul className='mt-40'>
+          <li className='py-5 px-5'>
+            <Link to="register" className='font-semibold text-white hover:text-pink-600'>ลงทะเบียนนักศึกษา</Link>
           </li>
 
-          <li className='border-b-4 border-white py-5 px-5'>
-            <Link to="check-books" className='font-semibold hover:text-pink-600'>ตรวจสอบหนังสือ</Link>
+          <li className='py-5 px-5'>
+            <Link to="check-books" className='font-semibold text-white hover:text-pink-600'>ตรวจสอบหนังสือ</Link>
           </li>
 
-          <li className='border-b-4 border-white py-5 px-5'>
-            <Link to="check-payment" className='font-semibold hover:text-pink-600'>ตรวจสอบชำระเงิน</Link>
+          <li className='py-5 px-5'>
+            <Link to="check-payment" className='font-semibold text-white hover:text-pink-600'>ตรวจสอบชำระเงิน</Link>
           </li>
 
         </ul>
-        <button
+        
+        <div className='flex flex-col mt-auto'>
+          <div className="flex items-center justify-center gap-2 py-2   bg-white rounded-sm" style={{ fontFamily: 'Superstore, sans-serif' }}>
+            <h3 className="text-sm font-bold">admin</h3>
+            <p className="w-0.5 h-[20px] bg-black" />
+            <h3 className="text-sm font-bold">101</h3>
+          </div>
+          <button
           onClick={handleLogout} 
-          className='p-4 bg-red-500 text-white rounded-xl transition-colors hover:bg-red-700'>Logout</button>
+          className="flex items-center justify-center gap-2 py-2 mt-4 text-white bg-red-600 rounded-sm "style={{ fontFamily: 'Superstore, sans-serif' }}>
+            Logout</button>
+        </div>
       </div>
 
       {/* Navbar and main content */}
       
       <div className='w-[85%] h-screen bg-gray-100 p-5 overflow-auto'>
         
-        <nav className='bg-[#D9D14378] w-full h-[80px] flex justify-between items-center px-10'>
-          <img src={UTCC} className="h-full w-auto object-contain"/>
-          <h3 className='text-xl font-semibold'>{userName}</h3>
-        </nav>
-
+      <HeadAdmin/>
         {/* แสดงเนื้อตาม route ภายใน admin */}
         <div>
 
